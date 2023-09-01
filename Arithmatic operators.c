364acc0eb5928ce885a91b485b74705800fca544
PAVANSAI KONGA <116155346+C1H2I3R4U5@users.noreply.github.com>
2023-09-01 16:57:03 +0530

    Add files via upload

---
+++ Arithmatic operators.c
@@ -0,0 +1,23 @@
+/*This program is to print the arithematic operators*/
+#include<stdio.h>
+main()
+{
+    /*variables declaration*/
+    int a=10,b=20;
+
+    /*Addition*/
+    printf("a+b is : %d",(a+b));
+
+    /*Subtraction*/
+    printf("\na-b is : %d",(a-b));
+
+    /*Multiplication*/
+    printf("\na*b is : %d",(a*b));
+
+    /*Division*/
+    printf("\na/b is : %d",(a/b));
+
+    /*Modulo Division*/
+    printf("\na%%b is : %d",(a%b));
+}
+
