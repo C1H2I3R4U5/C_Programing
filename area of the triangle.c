d1f841c02da5dadeeb06beb28ebd3a079c18a387
PAVANSAI KONGA <116155346+C1H2I3R4U5@users.noreply.github.com>
2023-09-01 16:55:34 +0530

    Add files via upload

---
+++ area of the triangle.c
@@ -0,0 +1,15 @@
+/*This program is  find the area of the Triangle*/
+#include<stdio.h>
+main()
+{
+    /*variables declaration*/
+    int l=10,b=10,a;
+
+    /*find the area*/
+    a=l*b;
+
+    /*print the result*/
+    printf("%d",a);
+
+
+}
