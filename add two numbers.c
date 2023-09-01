04ffe445199845d4a30956d91065be6d5fc95d39
PAVANSAI KONGA <116155346+C1H2I3R4U5@users.noreply.github.com>
2023-09-01 16:53:21 +0530

    Add files via upload
    #C programing

---
+++ add two numbers.c
@@ -0,0 +1,20 @@
+/* This Program is to print the sum of two numbers */
+#include<stdio.h>
+main()
+{
+    /*variables declaration*/
+   int a,b,result;
+   a=10;
+   b=20;
+   /*finding the sum of two variables*/
+   result=a+b;
+   /*printing the result*/
+   printf("%d",result);
+
+}
+
+
+
+
+
+
