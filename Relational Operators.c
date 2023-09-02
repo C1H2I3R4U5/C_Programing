6e163a2dfdfcdc56a5346542d56f7915fbafdc56
PAVANSAI KONGA <116155346+C1H2I3R4U5@users.noreply.github.com>
2023-09-02 13:55:48 +0530

    Add files via upload

---
+++ Relational Operators.c
@@ -0,0 +1,33 @@
+/* Program to demonstrate the operators in c*/
+#include<stdio.h>
+main()
+{
+    /*variables Declaration*/
+    int a=10,b=20,c=30;
+
+    /*<*/
+    printf("a<b is : %d",(a<b));
+    printf("\na<c is : %d",(a<c));
+
+    /*<=*/
+    printf("\na<=b is : %d",(a<=b));
+    printf("\na<=c is : %d",(a<=c));
+
+    /*>*/
+    printf("\na>b is : %d",(a>b));
+    printf("\na>c is : %d",(a>c));
+
+    /*>=*/
+    printf("\na>=b is : %d",(a>=b));
+    printf("\na>=c is : %d",(a>=c));
+
+    /*==*/
+    printf("\na==b is : %d",(a==b));
+    printf("\na==c is : %d",(a==c));
+
+    /*!=*/
+    printf("\na!=b is : %d",(a!=b));
+    printf("\na!=c is : %d",(a!=c));
+
+
+}
