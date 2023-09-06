001bcc3693d861f1d83a2f153ee950b4d4b2aa0f
PAVANSAI KONGA <116155346+C1H2I3R4U5@users.noreply.github.com>
2023-09-06 15:58:58 +0530

    Add files via upload

---
+++ bitwise operators.c
@@ -0,0 +1,27 @@
+/* Program to demonstrate bitwise operator*/
+#include<stdio.h>
+main()
+{
+    /*variable declaration*/
+    int a=12,b=4;
+
+
+    /*bitwise and*/
+    printf("a&b is : %d",(a&b));
+
+    /*bitwise lnclusive or*/
+
+    printf("\na|b is : %d",(a|b));
+
+    /*bitwise exclusive or*/
+    printf("\na^b is : %d",(a^b));
+
+    /*bitwise liftshift*/
+    printf("\na<<b is : %d",(a<<b));
+
+    /*bitwise rightshift*/
+    printf("\na>>b is : %d",(a>>b));
+
+
+
+}
