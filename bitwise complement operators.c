53471f5673e4f72ee37963e14e8020ca37b40062
PAVANSAI KONGA <116155346+C1H2I3R4U5@users.noreply.github.com>
2023-09-06 15:59:21 +0530

    Add files via upload

---
+++ bitwise complement operators.c
@@ -0,0 +1,14 @@
+/*Program to demonstrate bitwise complement operator*/
+#include<stdio.h>
+main()
+{
+  int a;
+
+  printf("Enter the value of a :");
+  scanf("%d",&a);
+
+  printf("\nComplement of a is : %d",~a);
+
+
+  return(0);
+}
