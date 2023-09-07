692b88277eaa87c81ed70248124c670d0b276240
PAVANSAI KONGA <116155346+C1H2I3R4U5@users.noreply.github.com>
2023-09-07 18:10:26 +0530

    Add files via upload

---
+++ even or odd using ocnditional operator.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+    int a,b;
+
+
+    printf("enter the number of a : ");
+    scanf("%d",a);
+
+    printf("enter the number of b : ");
+    scanf("%d",b);
+
+    if(a>20)
+    {
+       printf( "a is larger");
+    }
+     else
+     {
+         printf("b is larger");
+     }
+
+
+     return(0);
+
+
+}
